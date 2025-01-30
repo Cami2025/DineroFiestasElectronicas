6b01b36a8d3b9f2eea77c7fccf6393a8ed04c1cb
Cami2025 <camiartificial2025@gmail.com>
2025-01-30 19:04:22 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyA4ZOaSkduTmMNmMQ-B1UtCDJtSK7JU9hA",
+  authDomain: "fiestas-a5462.firebaseapp.com",
+  databaseURL: "https://fiestas-a5462-default-rtdb.firebaseio.com",
+  projectId: "fiestas-a5462",
+  storageBucket: "fiestas-a5462.firebasestorage.app",
+  messagingSenderId: "772543336072",
+  appId: "1:772543336072:web:b88c7f08a89f1dd3dcd93d",
+  measurementId: "G-317ZHZ47NW"
 };
 
-// Inicializa Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
-
+const analytics = getAnalytics(app);
